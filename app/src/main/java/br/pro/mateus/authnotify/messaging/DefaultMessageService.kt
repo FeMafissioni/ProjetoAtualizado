8827f583d9bbefe27b1e7fd453c4979e416808ac
EricRAbib <eric.ra1@puccampinas.edu.br>
2023-06-12 20:43:11 -0300

    notificacao

--- app/src/main/java/br/pro/mateus/authnotify/messaging/DefaultMessageService.kt
+++ app/src/main/java/br/pro/mateus/authnotify/messaging/DefaultMessageService.kt
@@ -8,9 +8,9 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import br.pro.mateus.authnotify.MainActivity
 import br.pro.mateus.authnotify.R
 import br.pro.mateus.authnotify.datastore.UserPreferencesRepository
+import br.pro.mateus.authnotify.emergency.EmergencyFragment
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -25,10 +25,9 @@ class DefaultMessageService : FirebaseMessagingService() {
      */
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val msgData = remoteMessage.data
-        val msg = msgData["text"]
-        showNotification(msg!!)
+        val msg = msgData["Emergency"]
+        showEmergencyNotification(msg!!, msgData)
     }
-
     override fun onNewToken(token: String) {
         Log.d("DefaultMessageService", "Refreshed token: $token")
         sendRegistrationToServer(token)
@@ -56,8 +55,14 @@ class DefaultMessageService : FirebaseMessagingService() {
      * você poderá abrir uma ou outra activity
      * ou enviar um parametro na Intent para tratar qual fragment abrir.(desafio para vc fazer)
      */
-    private fun showNotification(messageBody: String) {
-        val intent = Intent(this, MainActivity::class.java)
+    private fun showEmergencyNotification(messageBody: String, messageData: Map<String, String>) {
+        val intent = Intent(this, EmergencyFragment::class.java)
+        intent.action = "actionstring" + System.currentTimeMillis()
+        intent.putExtra("nome", messageData["nome"])
+        intent.putExtra("telefone", messageData["telefone"])
+        intent.putExtra("fotos", messageData["fotos"])
+        intent.putExtra("status", messageData["status"])
+        intent.putExtra("id", messageData["id"])
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
             PendingIntent.FLAG_IMMUTABLE)
