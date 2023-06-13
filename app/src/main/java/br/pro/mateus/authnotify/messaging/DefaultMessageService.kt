ae978e71d7a0342f089a53666c42ea63b53f8dfd
mathewsboscolo <mathewsboscolo@outlook.com>
2023-06-12 23:26:59 -0300

    feat: arrumei as notificações e testei o click delas

--- app/src/main/java/br/pro/mateus/authnotify/messaging/DefaultMessageService.kt
+++ app/src/main/java/br/pro/mateus/authnotify/messaging/DefaultMessageService.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import br.pro.mateus.authnotify.MainActivity
 import br.pro.mateus.authnotify.R
 import br.pro.mateus.authnotify.datastore.UserPreferencesRepository
 import br.pro.mateus.authnotify.emergency.EmergencyFragment
@@ -25,11 +26,14 @@ class DefaultMessageService : FirebaseMessagingService() {
      */
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val msgData = remoteMessage.data
-        val msg = msgData["Emergency"]
+        val msg = msgData["nome"]
+
         showEmergencyNotification(msg!!, msgData)
     }
     override fun onNewToken(token: String) {
+/*
         Log.d("DefaultMessageService", "Refreshed token: $token")
+*/
         sendRegistrationToServer(token)
     }
 
@@ -56,17 +60,23 @@ class DefaultMessageService : FirebaseMessagingService() {
      * ou enviar um parametro na Intent para tratar qual fragment abrir.(desafio para vc fazer)
      */
     private fun showEmergencyNotification(messageBody: String, messageData: Map<String, String>) {
-        val intent = Intent(this, EmergencyFragment::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         intent.action = "actionstring" + System.currentTimeMillis()
         intent.putExtra("nome", messageData["nome"])
         intent.putExtra("telefone", messageData["telefone"])
-        intent.putExtra("fotos", messageData["fotos"])
+        intent.putExtra("foto", messageData["foto"])
+
+/* tem que adicionar na function pra poder puxar
+
         intent.putExtra("status", messageData["status"])
-        intent.putExtra("id", messageData["id"])
+*/
+        intent.putExtra("emergenciaID", messageData["id"])
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
             PendingIntent.FLAG_IMMUTABLE)
         val channelId = getString(R.string.default_notification_channel_id)
+
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_check)
@@ -75,6 +85,7 @@ class DefaultMessageService : FirebaseMessagingService() {
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         // Since android Oreo notification channel is needed.
         val channel = NotificationChannel(channelId,
